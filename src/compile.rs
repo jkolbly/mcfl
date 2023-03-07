569af3cd32ed721e14371c7df1e0b8d32c5b2fdb
Jason Kolbly <jason@rscheme.org>
2023-03-06 20:26:11 -0600

    Change compile function variable name to ast

--- src/compile.rs
+++ src/compile.rs
@@ -1,7 +1,7 @@
 use crate::{ast::ASTNode, error::CompileError, ir::IR, tree::Tree};
 
 /// Generate an IR (intermediate representation) from an AST (abstract syntax tree)
-pub fn compile(tree: &Tree<ASTNode>) -> Result<IR, CompileError> {
+pub fn compile(ast: &Tree<ASTNode>) -> Result<IR, CompileError> {
     let mut ir = IR::new();
 
     Ok(ir)
