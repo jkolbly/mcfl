087cc613d4895b32422e15bb6edf5c230719499e
Jason Kolbly <jason@rscheme.org>
2023-05-21 22:30:09 -0500

    Create backbone of mir

---
+++ src/mir.rs
@@ -0,0 +1,34 @@
+use crate::ast::{StringContext, VarType};
+
+/// A node in the MIR (MCFL intermediate representation)
+///
+/// The MIR is a tree lying between the MCFL code and IR.
+pub struct MIRNode {
+    /// The type of node and associated data
+    pub node_type: MIRNodeType,
+
+    /// The string context of this node, for writing useful error messages
+    pub context: StringContext,
+}
+
+/// Stores the types of MIR nodes and their associated data
+pub enum MIRNodeType {
+    /// The root of a program. Should only appear once in the tree
+    Program,
+
+    /// A function or mcfunction that is not recursive
+    Function {
+        name: String,
+        params: Vec<Variable>,
+        return_var: Variable,
+    },
+}
+
+/// A variable with an identifier and type
+pub struct Variable {
+    /// The variable name, as will appear in the final datapack
+    name: String,
+
+    /// The variable's type
+    var_type: VarType,
+}
